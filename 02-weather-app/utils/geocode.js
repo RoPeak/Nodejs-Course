9273ee677424bb1249be1fc06e19880b57824db5
RoPeak <Ronanpeacock2004@gmail.com>
2024-07-20 13:40:35 +0100

    Section #6 Complete

---
+++ 02-weather-app/utils/geocode.js
@@ -0,0 +1,30 @@
+const request = require("request");
+
+// Method takes in the API URL with a real world address.
+// It properly concatenates these two things together and then makes the API call.
+// Method either returns an error or the placename, latitude, and longitude of the given address
+const geocode = (geoURL, address, callback) => {
+  // Add address to geoURL, properly encoded to prevent errors
+  const requestURL = geoURL + "&q=" + encodeURIComponent(address);
+
+  // Make request to mapbox.com
+  request({ url: requestURL, json: true }, (error, { body }) => {
+    // Error handling
+    if (error) {
+      // On error, return this to callback function to appropriately handle it
+      callback("Unable to connect to geolocation API.");
+    } else if (body.features.length === 0) {
+      // If nothing is returned, again return this to the callback function
+      callback("Unable to find location, please try again.");
+    } else {
+      // If request works then return place name with latitude and longitude
+      callback(undefined, {
+        location: body.features[0].properties.full_address,
+        latitude: body.features[0].geometry.coordinates[1],
+        longitude: body.features[0].geometry.coordinates[0],
+      });
+    }
+  });
+};
+
+module.exports = geocode;
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 Following "The Complete Node.js Developer Course (3rd Edition)" by Andrew Mead
+
+02-weather-app requires valid weather and geocoding API keys to work.
+These can be generated at https://www.weatherapi.com and https://www.mapbox.com
+
+I recommend putting the key in a .env file like below:
+WEATHER_API_KEY=your_api_key_here
+GEOCODING_API_KEY=your_api_key_here
+
+You can then use the dotenv package to load and access the key.
