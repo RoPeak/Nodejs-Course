5bcfb7267b8ed1a7b54230373dc79f510a3f8734
RoPeak <Ronanpeacock2004@gmail.com>
2024-07-21 16:55:15 +0100

    Section #8 Complete

--- 03-weather-web-server/public/js/app.js
+++ 03-weather-web-server/public/js/app.js
@@ -1,1 +1,35 @@
-console.log("Client side javascript file is loaded!");
+/* Select page elements */
+const weatherForm = document.querySelector("form");
+const searchElement = document.querySelector("input");
+const locationPara = document.querySelector("#message-location");
+const forecastPara = document.querySelector("#message-forecast");
+
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const userLocation = searchElement.value;
+
+  locationPara.textContent = "Loading...";
+  forecastPara.textContent = "";
+
+  fetch("http://localhost:3000/weather?address=" + userLocation).then(
+    (response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          console.log(data.error);
+          locationPara.textContent =
+            "Sorry, an error has occurred: '" + data.error + "'";
+        } else {
+          console.log(data);
+          locationPara.textContent =
+            "Location: " +
+            data.location.short +
+            " (" +
+            data.location.full +
+            ")";
+          forecastPara.textContent = "Forecast: " + data.forecast.summary;
+        }
+      });
+    }
+  );
+});
